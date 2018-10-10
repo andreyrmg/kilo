de7c7f546420360c26a31cf8ec0d2d0c3a21d591
Andrey Manzharov <manzharov@krista.ru>
2018-10-10 16:01:41 +0300

    window size, append buffer

--- src/main.rs
+++ src/main.rs
@@ -5,51 +5,45 @@ mod os;
 const CTRL: u8 = 0x1f;
 const CTRL_Q: u8 = CTRL & b'q';
 
-#[macro_use]
-mod vt100 {
-    macro_rules! write_csi {
-        ($cmd:expr) => {
-            os::target::write(concat!("\x1b[", $cmd))?
-        };
-    }
-    macro_rules! erase_in_display {
-        (all) => {
-            write_csi!("2J")
-        };
-    }
-    macro_rules! cursor_position {
-        () => {
-            write_csi!("H")
-        };
-    }
-}
-
 struct Editor {
-    _orig_term_attr: os::target::OriginalTerminalAttributes,
+    screen_rows: u16,
+    screen_cols: u16,
+    term: os::target::Terminal,
 }
 
 impl Editor {
     fn new() -> Result<Editor, Box<Error>> {
+        let term = os::target::Terminal::new_raw_mode()?;
+        let (rows, cols) = term.get_window_size()?;
         Ok(Editor {
-            _orig_term_attr: os::target::enable_raw_mode()?,
+            screen_rows: rows,
+            screen_cols: cols,
+            term: term,
         })
     }
 
-    fn draw_rows(&self) -> Result<(), Box<Error>> {
-        for _ in 0..24 {
-            os::target::write("~\r\n")?;
+    fn draw_rows(&mut self) {
+        for i in 0..self.screen_rows {
+            self.term.push("~");
+            self.term.erase_in_line();
+            if i < self.screen_rows - 1 {
+                self.term.push("\r\n");
+            }
         }
-        Ok(())
     }
 
-    fn refresh_screen(&self) -> Result<(), Box<Error>> {
-        erase_in_display!(all);
-        cursor_position!();
+    fn refresh_screen(&mut self) -> Result<(), Box<Error>> {
+        self.term.begin();
 
-        self.draw_rows()?;
+        self.term.hide_cursor();
+        self.term.move_cursor();
 
-        cursor_position!();
+        self.draw_rows();
 
+        self.term.move_cursor();
+        self.term.show_cursor();
+
+        self.term.end()?;
         Ok(())
     }
 
@@ -57,9 +51,11 @@ impl Editor {
         loop {
             self.refresh_screen()?;
             match os::target::read()? {
-                CTRL_Q => {
-                    erase_in_display!(all);
-                    cursor_position!();
+                Some(CTRL_Q) => {
+                    self.term.begin();
+                    self.term.erase_in_display();
+                    self.term.move_cursor();
+                    self.term.end()?;
                     return Ok(());
                 }
                 _ => (),
--- src/os.rs
+++ src/os.rs
@@ -33,15 +33,44 @@ pub mod unix {
         unsafe { *libc::__errno_location() }
     }
 
-    pub fn read() -> Result<u8, SysError> {
+    pub fn read() -> Result<Option<u8>, SysError> {
+        let mut c = unsafe { mem::uninitialized() };
+        let res =
+            unsafe { libc::read(libc::STDIN_FILENO, &mut c as *mut _ as *mut libc::c_void, 1) };
+        match res {
+            1 => Ok(Some(c)),
+            0 => Ok(None),
+            -1 if errno() == libc::EAGAIN => Ok(None),
+            _ => Err(SysError::last("read")),
+        }
+    }
+
+    pub fn read_match(b: u8) -> Result<(), SysError> {
+        let mut c: u8 = unsafe { mem::uninitialized() };
+        let res =
+            unsafe { libc::read(libc::STDIN_FILENO, &mut c as *mut _ as *mut libc::c_void, 1) };
+        match res {
+            1 if c == b => Ok(()),
+            1 => Err(SysError(
+                libc::EIO,
+                format!("read_match: expected b'{}' but read b'{}'", b, c),
+            )),
+            0 => Err(SysError(
+                libc::EIO,
+                format!("read_match: expected b'{}' but read nothing", b),
+            )),
+            _ => Err(SysError::last("read_match")),
+        }
+    }
+
+    fn read_exact() -> Result<u8, SysError> {
         let mut c = unsafe { mem::uninitialized() };
         let res =
             unsafe { libc::read(libc::STDIN_FILENO, &mut c as *mut _ as *mut libc::c_void, 1) };
         match res {
             1 => Ok(c),
-            0 => Ok(0),
-            -1 if errno() == libc::EAGAIN => Ok(0),
-            _ => Err(SysError::last("read")),
+            0 => Err(SysError(libc::EIO, "read_exact: read nothing".to_string())),
+            _ => Err(SysError::last("read_exact")),
         }
     }
 
@@ -70,31 +99,153 @@ pub mod unix {
         }
     }
 
-    pub struct OriginalTerminalAttributes {
-        attr: libc::termios,
-    }
+    #[macro_use]
+    mod vt100 {
+        use super::{read_exact, read_match, write, SysError};
 
-    impl Drop for OriginalTerminalAttributes {
-        fn drop(&mut self) {
-            tcsetattr(&self.attr).unwrap();
+        macro_rules! csi {
+            ($cmd:expr) => {
+                concat!("\x1b[", $cmd)
+            };
+        }
+        macro_rules! cursor_forward {
+            ($n:expr) => {
+                csi!(concat!($n, "C"))
+            };
+        }
+        macro_rules! cursor_down {
+            ($n:expr) => {
+                csi!(concat!($n, "B"))
+            };
+        }
+        macro_rules! cursor_position {
+            () => {
+                csi!("H")
+            };
+        }
+        macro_rules! erase_in_display {
+            () => {
+                csi!("2J")
+            };
+        }
+        macro_rules! erase_in_line {
+            () => {
+                csi!("K")
+            };
+        }
+        macro_rules! report_device_status {
+            (active_position) => {
+                csi!(concat!("6n"))
+            };
+        }
+        macro_rules! set_mode {
+            (hide_cursor) => {
+                csi!("?25l")
+            };
+            (show_cursor) => {
+                csi!("?25h")
+            };
+        }
+
+        pub fn get_cursor_position() -> Result<(u16, u16), SysError> {
+            write(concat!(
+                cursor_forward!(999),
+                cursor_down!(999),
+                report_device_status!(active_position)
+            ))?;
+            read_match(b'\x1b')?;
+            read_match(b'[')?;
+            let mut rows = 0u16;
+            loop {
+                match read_exact()? {
+                    b';' => break,
+                    b => rows = rows * 10 + (b - b'0') as u16,
+                }
+            }
+            let mut cols = 0u16;
+            loop {
+                match read_exact()? {
+                    b'R' => break,
+                    b => cols = cols * 10 + (b - b'0') as u16,
+                }
+            }
+            Ok((rows, cols))
         }
     }
 
-    pub fn enable_raw_mode() -> Result<OriginalTerminalAttributes, SysError> {
-        let attr = tcgetattr()?;
+    pub struct Terminal {
+        orig: libc::termios,
+        buf: String,
+    }
 
-        let mut raw = attr;
+    impl Terminal {
+        pub fn new_raw_mode() -> Result<Terminal, SysError> {
+            let orig = tcgetattr()?;
 
-        raw.c_iflag &= !(libc::BRKINT | libc::ICRNL | libc::INPCK | libc::ISTRIP | libc::IXON);
-        raw.c_oflag &= !libc::OPOST;
-        raw.c_cflag |= libc::CS8;
-        raw.c_lflag &= !(libc::ECHO | libc::ICANON | libc::IEXTEN | libc::ISIG);
-        raw.c_cc[libc::VMIN] = 0;
-        raw.c_cc[libc::VTIME] = 1;
+            let mut raw = orig;
 
-        tcsetattr(&raw)?;
+            raw.c_iflag &= !(libc::BRKINT | libc::ICRNL | libc::INPCK | libc::ISTRIP | libc::IXON);
+            raw.c_oflag &= !libc::OPOST;
+            raw.c_cflag |= libc::CS8;
+            raw.c_lflag &= !(libc::ECHO | libc::ICANON | libc::IEXTEN | libc::ISIG);
+            raw.c_cc[libc::VMIN] = 0;
+            raw.c_cc[libc::VTIME] = 1;
 
-        Ok(OriginalTerminalAttributes { attr: attr })
+            tcsetattr(&raw)?;
+
+            Ok(Terminal {
+                orig: orig,
+                buf: String::new(),
+            })
+        }
+
+        pub fn get_window_size(&self) -> Result<(u16, u16), SysError> {
+            let ws: libc::winsize = unsafe { mem::uninitialized() };
+            if unsafe { libc::ioctl(libc::STDOUT_FILENO, libc::TIOCGWINSZ, &ws) } == -1 {
+                write(concat!(cursor_forward!(999), cursor_down!(999)))?;
+                vt100::get_cursor_position()
+            } else {
+                Ok((ws.ws_row, ws.ws_col))
+            }
+        }
+
+        pub fn begin(&mut self) {
+            self.buf = String::new();
+        }
+
+        pub fn end(&self) -> Result<(), SysError> {
+            write(&self.buf)
+        }
+
+        pub fn erase_in_display(&mut self) {
+            self.buf.push_str(erase_in_display!());
+        }
+
+        pub fn erase_in_line(&mut self) {
+            self.buf.push_str(erase_in_line!());
+        }
+
+        pub fn hide_cursor(&mut self) {
+            self.buf.push_str(set_mode!(hide_cursor))
+        }
+
+        pub fn show_cursor(&mut self) {
+            self.buf.push_str(set_mode!(show_cursor))
+        }
+
+        pub fn move_cursor(&mut self) {
+            self.buf.push_str(cursor_position!())
+        }
+
+        pub fn push(&mut self, s: &str) {
+            self.buf.push_str(s);
+        }
+    }
+
+    impl Drop for Terminal {
+        fn drop(&mut self) {
+            tcsetattr(&self.orig).unwrap();
+        }
     }
 
 }
